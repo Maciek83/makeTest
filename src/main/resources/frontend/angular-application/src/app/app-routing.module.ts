2eaa33f73dfa5b7b06a1cfc2cd101df352609e60
MaciejGosciminski <zimneognie@gmail.com>
2020-06-27 17:02:16 +0200

    centering elements

--- src/main/resources/frontend/angular-application/src/app/app-routing.module.ts
+++ src/main/resources/frontend/angular-application/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule} from '@angular/router'
 import { QuestionsComponent } from './questions/questions.component';
 import { HomeComponent } from './home/home.component';
+import { AddquestionComponent } from './questions/addquestion/addquestion.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component:HomeComponent},
-  {path: 'question', component:QuestionsComponent}
+  {path: 'question', component:QuestionsComponent},
+  {path: 'addquestion', component:AddquestionComponent}
 ]
 
 
