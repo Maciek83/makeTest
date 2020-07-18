5cc22812b924d3617a4ab31a35844b8c6b425ae5
MaciejGosciminski <zimneognie@gmail.com>
2020-07-18 22:39:40 +0200

    small changes

--- src/main/java/com/gosciminski/testsapp/exceptions/QuestionNotFoundException.java
+++ src/main/java/com/gosciminski/testsapp/exceptions/QuestionNotFoundException.java
@@ -3,6 +3,6 @@ package com.gosciminski.testsapp.exceptions;
 public class QuestionNotFoundException extends RuntimeException {
     
     public QuestionNotFoundException(Long id){
-        super("Could not find question: " + id);
+        super("Could not find question with id: " + id);
     }
 }
