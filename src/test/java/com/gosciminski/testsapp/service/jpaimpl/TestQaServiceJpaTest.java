05925805d1c5a536f3e524703dcea5ebed36cf12
MaciejGosciminski <zimneognie@gmail.com>
2020-07-20 12:35:33 +0200

    adding test for testing test service

--- src/test/java/com/gosciminski/testsapp/service/jpaimpl/TestQaServiceJpaTest.java
+++ src/test/java/com/gosciminski/testsapp/service/jpaimpl/TestQaServiceJpaTest.java
@@ -4,16 +4,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import com.gosciminski.testsapp.converter.TestQaToTestQaDisplayDto;
+import com.gosciminski.testsapp.dto.create.TestQaCreateDto;
 import com.gosciminski.testsapp.dto.display.TestQaDisplayDto;
+import com.gosciminski.testsapp.dto.edit.TestQaEditDto;
+import com.gosciminski.testsapp.exceptions.QuestionNotFoundException;
+import com.gosciminski.testsapp.exceptions.TestQaNotFoundException;
+import com.gosciminski.testsapp.exceptions.TestQaZeroQuestionsException;
 import com.gosciminski.testsapp.exceptions.UserNotFoundException;
+import com.gosciminski.testsapp.model.Question;
 import com.gosciminski.testsapp.model.TestQa;
 import com.gosciminski.testsapp.model.User;
 import com.gosciminski.testsapp.repisitory.TestQaRepository;
@@ -25,6 +34,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -44,6 +54,10 @@ public class TestQaServiceJpaTest {
 
     @InjectMocks
     TestQaServiceJpa testQaServiceJap;
+
+    @Spy
+    @InjectMocks
+    TestQaServiceJpa testQaServiceJapSpy;
 
     List<TestQa> tests = new LinkedList<>();
 
@@ -93,5 +107,168 @@ public class TestQaServiceJpaTest {
         Assertions.assertThrows(UserNotFoundException.class, () ->{
             testQaServiceJap.findAllByUser();
         });
+    }
+
+    @Test
+    public void save_shouldThrowTestQaZeroQuestionsException(){
+        TestQaCreateDto createDto = new TestQaCreateDto();
+
+        Assertions.assertThrows(TestQaZeroQuestionsException.class, () ->{
+            testQaServiceJap.save(createDto);
+        });
+
+    }
+
+    @Test
+    public void save_shoultThrowQuestionNotFoundException(){
+        TestQaCreateDto createDto = new TestQaCreateDto();
+        createDto.getQuestionsIds().add(1L);
+        createDto.getQuestionsIds().add(2L);
+
+        when(questionServiceMock.findById(any(Long.class))).thenThrow(QuestionNotFoundException.class);
+
+        Assertions.assertThrows(QuestionNotFoundException.class, () ->{
+            testQaServiceJap.save(createDto);
+        });
+    }
+
+    @Test
+    public void save_shouldSave(){
+        TestQaCreateDto createDto = new TestQaCreateDto();
+        createDto.getQuestionsIds().add(1L);
+        createDto.getQuestionsIds().add(2L);
+
+        when(questionServiceMock.findById(any(Long.class))).thenReturn(new Question());
+        when(testRepositoryMock.save(any(TestQa.class))).thenReturn(new TestQa());
+        when(testQaToTestQaDisplayDtoMock.convert(any(TestQa.class))).thenReturn(new TestQaDisplayDto());
+
+        TestQaDisplayDto result = testQaServiceJap.save(createDto);
+
+        assertNotNull(result);
+
+        verify(questionServiceMock, times(2)).findById(any(Long.class));
+        verify(testRepositoryMock, atLeastOnce()).save(any(TestQa.class));
+        verify(testQaToTestQaDisplayDtoMock, atLeastOnce()).convert(any(TestQa.class));
+    }
+
+    @Test
+    public void findById_shouldThrowTestQaNotFoundException(){
+        TestQa test = new TestQa();
+        test.setId(1L);
+        test.setUser(null);
+        User user = new User();
+
+        when(testRepositoryMock.findById(any(Long.class))).thenReturn(Optional.of(test));
+        when(userServiceMock.getUser()).thenReturn(user);
+
+        Assertions.assertThrows(TestQaNotFoundException.class, () -> {
+            testQaServiceJap.findById(1L);
+        });
+    }
+
+    @Test
+    public void findById_shouldFind(){
+        TestQa test = new TestQa();
+        test.setId(1L);
+        User user = new User();
+        test.setUser(user);
+
+        when(testRepositoryMock.findById(any(Long.class))).thenReturn(Optional.of(test));
+        when(userServiceMock.getUser()).thenReturn(user);
+        
+        TestQa result = testQaServiceJap.findById(1L);
+
+        assertNotNull(result);
+
+        verify(testRepositoryMock, atLeastOnce()).findById(any(Long.class));
+        verify(userServiceMock, atLeastOnce()).getUser();
+    }
+
+    @Test
+    public void findTestDisplayDtoById_shouldThrowTestQaNotFoundException(){
+
+        doThrow(TestQaNotFoundException.class).when(testQaServiceJapSpy).findById(1L);
+
+        Assertions.assertThrows(TestQaNotFoundException.class, () -> {
+            testQaServiceJapSpy.findTestDisplayDtoById(1L);
+        });
+    }
+
+    @Test
+    public void findTestDisplayDtoById_shouldFind(){
+
+        doReturn(new TestQa()).when(testQaServiceJapSpy).findById(1L);
+        when(testQaToTestQaDisplayDtoMock.convert(any(TestQa.class))).thenReturn(new TestQaDisplayDto());
+
+        TestQaDisplayDto result = testQaServiceJapSpy.findTestDisplayDtoById(1L);
+
+        assertNotNull(result);
+
+        verify(testQaServiceJapSpy, atLeastOnce()).findById(any(Long.class));
+        verify(testQaToTestQaDisplayDtoMock, atLeastOnce()).convert(any(TestQa.class));
+    }
+
+    @Test
+    public void update_shouldThrowTestQaNotFoundException(){
+        TestQaEditDto editDto = new TestQaEditDto();
+
+        doThrow(TestQaNotFoundException.class).when(testQaServiceJapSpy).findById(1L);
+
+        Assertions.assertThrows(TestQaNotFoundException.class, () -> {
+            testQaServiceJapSpy.update(1L, editDto);
+        });
+    }
+
+    @Test
+    public void update_shouldThrowTestQaZeroQuestionsException(){
+        TestQaEditDto editDto = new TestQaEditDto();
+
+        doReturn(new TestQa()).when(testQaServiceJapSpy).findById(1L);
+
+        Assertions.assertThrows(TestQaZeroQuestionsException.class, () -> {
+            testQaServiceJapSpy.update(1L, editDto);
+        });
+    }
+
+    @Test
+    public void update_shouldThrowQuestionNotFoundException(){
+        TestQaEditDto editDto = new TestQaEditDto();
+        editDto.getQuestionsIds().add(1L);
+        
+        doReturn(new TestQa()).when(testQaServiceJapSpy).findById(1L);
+        when(questionServiceMock.findById(any(Long.class))).thenThrow(QuestionNotFoundException.class);
+
+        Assertions.assertThrows(QuestionNotFoundException.class, () -> {
+            testQaServiceJapSpy.update(1L, editDto);
+        });
+    }
+
+    @Test
+    public void update_ShouldUpdate(){
+        TestQaEditDto editDto = new TestQaEditDto();
+        editDto.getQuestionsIds().add(1L);
+        editDto.getQuestionsIds().add(2L);
+
+        TestQa test = new TestQa();
+        Question question = new Question();
+        question.setId(3L);
+        Question question2 = new Question();
+        question2.setId(4L);
+        test.getQuestions().add(question);
+        test.getQuestions().add(question2);
+
+        doReturn(test).when(testQaServiceJapSpy).findById(1L);
+        when(questionServiceMock.findById(any(Long.class))).thenReturn(new Question());
+        when(testRepositoryMock.save(any(TestQa.class))).thenReturn(test);
+        when(testQaToTestQaDisplayDtoMock.convert(any(TestQa.class))).thenReturn(new TestQaDisplayDto());
+
+        TestQaDisplayDto result = testQaServiceJapSpy.update(1L, editDto);
+
+        assertNotNull(result);
+
+        verify(testQaServiceJapSpy, atLeastOnce()).findById(any(Long.class));
+        verify(questionServiceMock, times(4)).findById(any(Long.class));
+        verify(testRepositoryMock, atLeastOnce()).save(any(TestQa.class));
+        verify(testQaToTestQaDisplayDtoMock, atLeastOnce()).convert(any(TestQa.class));
     }
 }
